fd3fa4bf910ed89bc77c37c3729102d88da3d7d0
GrowlyX <growlyxdev@gmail.com>
2022-05-26 14:40:59 -0400

    Code cleanup

--- game/src/main/kotlin/gg/scala/bedwars/game/listener/BedwarsGameListener.kt
+++ game/src/main/kotlin/gg/scala/bedwars/game/listener/BedwarsGameListener.kt
@@ -12,7 +12,6 @@ import net.evilblock.cubed.util.CC
 import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.Sound
-import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
@@ -25,70 +24,117 @@ import org.bukkit.metadata.FixedMetadataValue
 @Listeners
 object BedwarsGameListener : Listener
 {
-    @Inject lateinit var plugin: ScalaBedwarsGame
+    @Inject
+    lateinit var plugin: ScalaBedwarsGame
 
     @EventHandler
-    fun onQuit(e: PlayerQuitEvent) {
-        val team = (CgsGameTeamService.getTeamOf(e.player) as BedwarsCgsGameTeam)
-        if (team.bedDestroyed) CgsGameDisqualificationHandler.disqualifyPlayer(e.player)
+    fun onQuit(event: PlayerQuitEvent)
+    {
+        val team = (CgsGameTeamService.getTeamOf(event.player) as BedwarsCgsGameTeam)
+
+        if (team.bedDestroyed)
+        {
+            CgsGameDisqualificationHandler.disqualifyPlayer(event.player)
+        }
     }
 
     @EventHandler
-    fun onRejoin(e: PlayerJoinEvent) {
-        val team = (CgsGameTeamService.getTeamOf(e.player) as BedwarsCgsGameTeam)
-        if (team.bedDestroyed) CgsGameDisqualificationHandler.disqualifyPlayer(e.player)
+    fun onRejoin(event: PlayerJoinEvent)
+    {
+        val team = (CgsGameTeamService.getTeamOf(event.player) as BedwarsCgsGameTeam)
+        if (team.bedDestroyed) CgsGameDisqualificationHandler.disqualifyPlayer(event.player)
     }
 
     @EventHandler
-    fun onPlace(e: BlockPlaceEvent) {
-        e.blockPlaced.setMetadata("placed", FixedMetadataValue(plugin, true))
+    fun onPlace(event: BlockPlaceEvent)
+    {
+        event.blockPlaced.setMetadata("placed", FixedMetadataValue(plugin, true))
     }
 
     @EventHandler
-    fun onBreak(e: BedwarsBedDestroyEvent) {
-        e.team.bedDestroyed = true
+    fun onBreak(event: BedwarsBedDestroyEvent)
+    {
+        event.team.bedDestroyed = true
 
         CgsGameEngine.INSTANCE.playSound(Sound.ENDERDRAGON_GROWL)
         CgsGameEngine.INSTANCE.sendMessage("")
         CgsGameEngine.INSTANCE.sendMessage(CC.B_WHITE + "Bed Destroyed")
         CgsGameEngine.INSTANCE.sendMessage("")
-        CgsGameEngine.INSTANCE.sendMessage("${e.team.name}${CC.RED}'s bed has been destroyed!")
-        if (e.destroyer != null) CgsGameEngine.INSTANCE.sendMessage(CC.RED + "Destroyed by " + e.destroyer.displayName)
+        CgsGameEngine.INSTANCE.sendMessage("${event.team.name}${CC.RED}'s bed has been destroyed!")
+
+        if (event.destroyer != null)
+        {
+            CgsGameEngine.INSTANCE.sendMessage(
+                CC.RED + "Destroyed by " + event.destroyer.displayName
+            )
+        }
+
         CgsGameEngine.INSTANCE.sendMessage("")
 
-        e.team.alive.filter { Bukkit.getPlayer(it) == null }.forEach {
+        event.team.alive.filter { Bukkit.getPlayer(it) == null }.forEach {
             CgsGameDisqualificationHandler.disqualifyPlayer(it)
         }
-        if (e.team.alive.mapNotNull { Bukkit.getPlayer(it) }.isEmpty()) {
-            e.team.broadcastElimination()
+
+        if (
+            event.team.alive
+                .mapNotNull { Bukkit.getPlayer(it) }
+                .isEmpty()
+        )
+        {
+            event.team.broadcastElimination()
         }
     }
 
     @EventHandler
-    fun onBreak(e: BlockBreakEvent) {
-        if (e.block.type == Material.BED_BLOCK) {
-            if (e.block.hasMetadata("team")) {
-                val meta = e.block.getMetadata("team")
-                BedwarsBedDestroyEvent(CgsGameTeamService.teams[meta[0].asInt()] as BedwarsCgsGameTeam, e.player).callEvent()
+    fun onBreak(event: BlockBreakEvent)
+    {
+        if (event.block.type == Material.BED_BLOCK)
+        {
+            if (event.block.hasMetadata("team"))
+            {
+                val meta = event.block.getMetadata("team")
+
+                BedwarsBedDestroyEvent(
+                    CgsGameTeamService.teams[meta[0].asInt()] as BedwarsCgsGameTeam,
+                    event.player
+                ).callEvent()
             }
-        } else if (!e.block.hasMetadata("placed")) {
-            e.player.sendMessage("${CC.RED}You can only break blocks placed by players!")
-            e.isCancelled = true
+        } else if (!event.block.hasMetadata("placed"))
+        {
+            event.player.sendMessage("${CC.RED}You can only break blocks placed by players!")
+            event.isCancelled = true
         }
     }
 
     @EventHandler
-    fun onDeath(e: PlayerDeathEvent) {
-        if (e.entity.hasMetadata("spectator")) return
-        val team = CgsGameTeamService.getTeamOf(e.entity) as BedwarsCgsGameTeam?
+    fun onDeath(event: PlayerDeathEvent)
+    {
+        if (
+            event.entity
+                .hasMetadata("spectator")
+        )
+        {
+            return
+        }
 
-        if (team != null) {
-            if (team.bedDestroyed) {
+        val team = CgsGameTeamService.getTeamOf(event.entity) as BedwarsCgsGameTeam?
+
+        if (team != null)
+        {
+            if (team.bedDestroyed)
+            {
                 CgsGameDisqualificationHandler.disqualifyPlayer(
-                    player = e.entity, broadcastNotification = true, setSpectator = true
+                    player = event.entity, broadcastNotification = true, setSpectator = true
                 )
-                if (team.alive.isEmpty()) team.broadcastElimination()
-            } else e.entity.teleport(team.spawnPoint)
+
+                if (team.alive.isEmpty())
+                {
+                    team.broadcastElimination()
+                }
+            } else
+            {
+                event.entity.teleport(team.spawnPoint)
+            }
         }
     }
 }
