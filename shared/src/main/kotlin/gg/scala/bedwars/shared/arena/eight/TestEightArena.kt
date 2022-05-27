dabce0fd956c4558eea993c619307d06d39bdc78
GrowlyX <growlyxdev@gmail.com>
2022-05-27 16:07:12 -0400

    Add temporary implementation of new mappings

--- shared/src/main/kotlin/gg/scala/bedwars/shared/arena/eight/TestEightArena.kt
+++ shared/src/main/kotlin/gg/scala/bedwars/shared/arena/eight/TestEightArena.kt
@@ -6,30 +6,55 @@ import org.bukkit.Material
 
 object TestEightArena : BedwarsArena() {
 
-    override val diamondGenerators: List<Location>
-        get() = mutableListOf(
-            buildSpawnPoint(5.0, 100.0, 0.0),
-            buildSpawnPoint(-5.0, 100.0, 0.0),
-            buildSpawnPoint(0.0, 100.0, 5.0),
-            buildSpawnPoint(0.0, 100.0, -5.0),
-        )
-    override val emeraldGenerators: List<Location>
-        get() = mutableListOf(buildSpawnPoint(0.0, 100.0, 0.0))
-    override val teamSpawnPoints: MutableMap<Int, Location>
-        get() = mutableMapOf(
-            1 to buildSpawnPoint(-8.0, 100.0, 15.0),
-            2 to buildSpawnPoint(8.0, 100.0, 15.0),
+    override val diamondGenerators = mutableListOf(
+        buildSpawnPoint(5.0, 100.0, 0.0),
+        buildSpawnPoint(-5.0, 100.0, 0.0),
+        buildSpawnPoint(0.0, 100.0, 5.0),
+        buildSpawnPoint(0.0, 100.0, -5.0),
+    )
 
-            3 to buildSpawnPoint(8.0, 100.0, -15.0),
-            4 to buildSpawnPoint(-8.0, 100.0, -15.0),
+    override val emeraldGenerators = mutableListOf(
+        buildSpawnPoint(0.0, 100.0, 0.0)
+    )
+
+    override val teamShopLocations = mutableMapOf(
+        0 to buildSpawnPoint(0.0, 100.0, 0.0)
+    )
+
+    override val teamGeneratorLocations = mutableMapOf(
+        0 to buildSpawnPoint(0.0, 100.0, 0.0)
+    )
+
+    override val teamItemUpgradeLocations = mutableMapOf(
+        0 to buildSpawnPoint(0.0, 100.0, 0.0)
+    )
+
+    override val teamBedLocations = mutableMapOf(
+        0 to buildSpawnPoint(0.0, 100.0, 0.0)
+    )
+
+    override val teamEnderChestLocations = mutableMapOf(
+        0 to buildSpawnPoint(0.0, 100.0, 0.0)
+    )
+
+    override val teamChestLocations = mutableMapOf(
+        0 to buildSpawnPoint(0.0, 100.0, 0.0)
+    )
+
+    override val teamSpawnPoints = mutableMapOf(
+        1 to buildSpawnPoint(-8.0, 100.0, 15.0),
+        2 to buildSpawnPoint(8.0, 100.0, 15.0),
+
+        3 to buildSpawnPoint(8.0, 100.0, -15.0),
+        4 to buildSpawnPoint(-8.0, 100.0, -15.0),
 
 
-            5 to buildSpawnPoint(15.0, 100.0, 8.0),
-            6 to buildSpawnPoint(15.0, 100.0, -8.0),
+        5 to buildSpawnPoint(15.0, 100.0, 8.0),
+        6 to buildSpawnPoint(15.0, 100.0, -8.0),
 
-            7 to buildSpawnPoint(-15.0, 100.0, 8.0),
-            8 to buildSpawnPoint(-15.0, 100.0, -8.0)
-        )
+        7 to buildSpawnPoint(-15.0, 100.0, 8.0),
+        8 to buildSpawnPoint(-15.0, 100.0, -8.0)
+    )
 
     override fun getDescription(): String {
         return "TEST - Bedwars Arena (EIGHT)"
